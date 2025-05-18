7d20cd18ca308f01f6e96d3aa19d49024d1ddfac
DanielHemmati <danialups1997@gmail.com>
2025-05-18 16:43:06 +0200

    add id for rendering on ui

--- config/nodes.php
+++ config/nodes.php
@@ -1,31 +1,45 @@
 <?php
 
+use Illuminate\Support\Str;
+
 /**
  * This is just the first version
+ * What category should i use for the nodes ?
+ * Is category the same as the type ?
  *
  */
 
 return [
-    'httpRequest.node' => [
-        'category' => 'API', // this is the category of the node
+    'HttpRequest.node' => [
+        'id' => 'httpRequest.node.' . Str::random(10),
+        'category' => 'API', // this is the category of the node, still not sure about naming
         'displayName' => 'HTTP Request',
-        'type' => 'httpRequest',
+        'type' => 'HttpRequest.node', // this helps for drag and drop
         'description' => 'Make an HTTP request',
         'icon' => '🌐',
         'tags' => ['http', 'request'],
         'inputs' => [
             'url' => [
-                'type' => 'string',
+                'id' => Str::random(10),
+                'type' => 'text', // it would be input of type text
+                'label' => 'URL',
                 'required' => true,
             ],
+            'method' => [
+                'id' => Str::random(10),
+                'type' => 'select',
+                'label' => 'Method',
+                'required' => true,
+                'options' => [
+                    'GET',
+                    'POST',
+                    'DELETE',
+                    'PUT',
+                    'PATCH',
+                    'HEAD',
+                    'OPTIONS',
+                ],
+            ],
         ],
-    ],
-    'input.node' => [
-        'category' => 'Input',
-        'displayName' => 'Input',
-        'type' => 'input',
-        'description' => 'Input node',
-        'icon' => '🔄',
-        'tags' => ['input'],
     ],
 ];
